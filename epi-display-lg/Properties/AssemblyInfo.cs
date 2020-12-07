39cc8bf45c19ad4785ddbf0d329e4da2ce937d5a
Andrew Welker <andrew.welker@gmail.com>
2020-12-07 14:05:41 -0700

    update workflows and add nuspec

--- epi-display-lg/Properties/AssemblyInfo.cs
+++ epi-display-lg/Properties/AssemblyInfo.cs
@@ -5,4 +5,6 @@
 [assembly: AssemblyProduct("Epi-Display-Lg")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyInformationalVersion("0.0.0-buildtype-build#")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("0.0.0-buildtype-build#")]
 
